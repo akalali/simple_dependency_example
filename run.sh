bdd8fd13384485cd8edd28103609ac13fde11b6c
Thomas Foerster <32761865+tfoerst3r@users.noreply.github.com>
2021-09-24 16:46:48 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+user=<users homedir>
+
 # compiling two_libraries
 cd ./libs/two_libraries
 mkdir build
 cmake -S . -B build/
 cmake --build build/
-cmake --install build/ --prefix /home/tfoerst/libs/cmake_ex_2libs
+cmake --install build/ --prefix /home/$user/libs/cmake_ex_2libs
 
 # compiling simple_libraries
 # it depends on two_libraries
 cd ..
 cd simple_library
 mkdir build
-cmake -S . -B build/ -D CMAKE_PREFIX_PATH="/home/tfoerst/libs/cmake_ex_2libs/cmake"
+cmake -S . -B build/ -D CMAKE_PREFIX_PATH="/home/$user/libs/cmake_ex_2libs/cmake"
 cmake --build build/
-cmake --install build/ --prefix /home/tfoerst/libs/cmake_ex_1lib
+cmake --install build/ --prefix /home/$user/libs/cmake_ex_1lib
 
 
 # compiling the executable
 cd ../../app/
 mkdir build
 cmake -S . -B build/
-cmake -S . -B build/ -D CMAKE_PREFIX_PATH="/home/tfoerst/libs/cmake_ex_2libs/cmake;/home/tfoerst/libs/cmake_ex_1lib/SimpleLibrary/cmake"
+cmake -S . -B build/ -D CMAKE_PREFIX_PATH="/home/$user/libs/cmake_ex_2libs/cmake;/home/$user/libs/cmake_ex_1lib/SimpleLibrary/cmake"
 cmake --build build/
 
 ./build/Main
